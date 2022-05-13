5e0b7255dcd6b3ad8cfbc9df7d0d8f0f2d9d4c47
William Iommi <william.iommi@gmail.com>
2022-05-14 01:21:27 +0200

    added hover behaviour to SmallTile Component

--- src/components/SmallTile.tsx
+++ src/components/SmallTile.tsx
@@ -1,4 +1,5 @@
-import { useMemo } from 'react';
+import { Button } from '@contentful/f36-components';
+import { memo, useMemo } from 'react';
 import { getElapsedTime } from '../lib/utils';
 
 interface SmallTileProps {
@@ -6,21 +7,43 @@ interface SmallTileProps {
   thumbnail?: string;
   title?: string;
   date?: string;
-  onClick: (id?: string) => void;
+  onSelect: (id?: string) => void;
+  onPreview: (id?: string) => void;
 }
 
-const SmallTile = ({ id, thumbnail, title, date, onClick }: SmallTileProps) => {
+const SmallTile = ({ id, thumbnail, title, date, onSelect, onPreview }: SmallTileProps) => {
+  console.log('smallTile render::');
   const formattedDate = useMemo(() => getElapsedTime(date), [date]);
+  const ctas = useMemo(() => {
+    return [
+      { label: 'PREVIEW', cb: () => onPreview(id) },
+      { label: 'SELECT', cb: () => onSelect(id) },
+    ];
+  }, [id]);
   return (
-    <button
-      onClick={() => onClick(id)}
-      className="flex max-w-[200px] cursor-pointer flex-col text-left"
-    >
-      <img src={thumbnail} alt={title} />
+    <div className="flex max-w-[200px] cursor-pointer flex-col text-left">
+      <div className="relative group">
+        <img
+          src={thumbnail}
+          alt={title}
+          className="group-hover:blur-sm transition-all duration-300"
+        />
+        <div className="absolute inset-0 bg-black/30 flex flex-col space-y-3 justify-center items-center opacity-0 group-hover:opacity-100 transition-all duration-300">
+          {ctas.map((btn) => (
+            <button
+              key={btn.label}
+              className="font-bold py-1 rounded-md border border-white text-white hover:text-black hover:bg-white w-24 transition-all"
+              onClick={btn.cb}
+            >
+              {btn.label}
+            </button>
+          ))}
+        </div>
+      </div>
       <h2 className="text-sm" dangerouslySetInnerHTML={{ __html: `${title}` }} />
       {formattedDate && <span className="text-xs text-gray-500">{formattedDate}</span>}
-    </button>
+    </div>
   );
 };
 
-export default SmallTile;
+export default memo(SmallTile);
