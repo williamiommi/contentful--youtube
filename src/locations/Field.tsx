bb6e83b245243ae5ceb0bdf62a7deaf6a010ec04
William Iommi <william.iommi@gmail.com>
2022-05-15 01:14:25 +0200

    working on ctas

--- src/locations/Field.tsx
+++ src/locations/Field.tsx
@@ -4,28 +4,28 @@ import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 import useYtApi from '../lib/hooks/useYtApi';
 import useAutoResizer from '../lib/hooks/useAutoResizer';
 import Tile from '../components/Tile';
+import { Button } from '@contentful/f36-components';
 
 const Field = () => {
   const sdk = useSDK<FieldExtensionSDK>();
   const [fieldValue, setFieldValue] = useState(sdk.field.getValue());
-  const { getVideosInfo, videos } = useYtApi(sdk.parameters.installation.apiKey);
+  const { getVideosInfo, videos, setVideos } = useYtApi(sdk.parameters.installation.apiKey);
   useAutoResizer();
 
-  // const clearField = async () => {
-  //   await sdk.field.removeValue();
-  //   setFieldValue(undefined);
-  //   setVideos(undefined);
-  //   sdk.entry.save();
-  // };
+  const clearField = async () => {
+    await sdk.field.removeValue();
+    setFieldValue(undefined);
+    setVideos(undefined);
+    sdk.entry.save();
+  };
 
   const setVideo = (id: string) => {
-    // Ipv_LWcdfC0
     sdk.field.setValue([id]);
     setFieldValue([id]);
     sdk.entry.save();
   };
 
-  const openDialog = async () => {
+  const selectVideos = async () => {
     const res = await sdk.dialogs.openCurrent({
       position: 'top',
       minHeight: '75vh',
@@ -44,29 +44,39 @@ const Field = () => {
 
   return (
     <>
-      {videos &&
-        videos.map((video) => (
-          <Tile
-            key={video.id}
-            videoId={video.id}
-            title={video.snippet.title}
-            description={video.snippet.description}
-            embedHtml={video.player.embedHtml}
-            viewCount={video.statistics.viewCount}
-            commentCount={video.statistics.commentCount}
-            likeCount={video.statistics.likeCount}
-            publishedAt={video.snippet.publishedAt}
-            channelThumbnail={video.channelInfo?.snippet.thumbnails.default?.url}
-            channelCustomUrl={video.channelInfo?.snippet.customUrl}
-            channelTitle={video.channelInfo?.snippet.title}
-          />
-        ))}
-      <button
-        className="flex items-center justify-center bg-sky-900 hover:bg-sky-700 text-white p-4"
-        onClick={openDialog}
-      >
-        Open Dialog
-      </button>
+      {videos && (
+        <div className="flex flex-col space-y-3">
+          {videos.map((video) => (
+            <>
+              <Tile
+                key={video.id}
+                videoId={video.id}
+                title={video.snippet.title}
+                description={video.snippet.description}
+                embedHtml={video.player.embedHtml}
+                viewCount={video.statistics.viewCount}
+                commentCount={video.statistics.commentCount}
+                likeCount={video.statistics.likeCount}
+                publishedAt={video.snippet.publishedAt}
+                channelThumbnail={video.channelInfo?.snippet.thumbnails.default?.url}
+                channelCustomUrl={video.channelInfo?.snippet.customUrl}
+                channelTitle={video.channelInfo?.snippet.title}
+              />
+            </>
+          ))}
+        </div>
+      )}
+      {videos && <div className="my-3 h-[1px] bg-gray-600"></div>}
+      <section className="space-x-2">
+        <Button variant="primary" className="font-bold" onClick={selectVideos}>
+          {videos ? 'CHANGE VIDEO' : 'SELECT A VIDEO'}
+        </Button>
+        {videos && (
+          <Button variant="negative" className="font-bold" onClick={clearField}>
+            REMOVE VIDEO
+          </Button>
+        )}
+      </section>
     </>
   );
 };
