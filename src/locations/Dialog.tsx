a8d46598c0bb11e8c6b32093c8c2c78c7a0629fd
William Iommi <william.iommi@gmail.com>
2022-05-14 18:31:01 +0200

    simple animation w/ framer motion

--- src/locations/Dialog.tsx
+++ src/locations/Dialog.tsx
@@ -1,9 +1,16 @@
-import { useRef } from 'react';
+import { useCallback, useRef } from 'react';
 import { DialogExtensionSDK } from '@contentful/app-sdk';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
-import { Button, TextInput } from '@contentful/f36-components';
+import { Button, Modal, TextInput, ModalLauncher } from '@contentful/f36-components';
 import useYtApi from '../lib/hooks/useYtApi';
 import SmallTile from '../components/SmallTile';
+import PreviewModal from '../components/PreviewModal';
+import { motion } from 'framer-motion';
+
+const item = {
+  hidden: { opacity: 0, y: -20 },
+  show: { opacity: 1, y: 0 },
+};
 
 const Dialog = () => {
   const sdk = useSDK<DialogExtensionSDK>();
@@ -18,43 +25,61 @@ const Dialog = () => {
     if (!yt.isFetching) yt._loadMore();
   };
 
+  const onSelectVideo = useCallback((id?: string) => {
+    sdk.close({ id });
+  }, []);
+
+  const onModalLauncher = useCallback((id?: string) => {
+    ModalLauncher.open(({ onClose }) => {
+      return <PreviewModal id={id} onClose={onClose} />;
+    });
+  }, []);
+
   return (
-    <div className="p-10">
-      <div className="flex items-center justify-center max-w-2xl mx-auto gap-x-2">
-        <TextInput ref={inputRef} className="w-10/12" placeholder="Search for a video..." />
-        <Button
-          variant="negative"
-          className="w-2/12 !font-bold"
-          onClick={onSearch}
-          isLoading={yt.isFetching}
-        >
-          {!yt.isFetching ? 'SEARCH' : ''}
-        </Button>
+    <>
+      <Modal.Header title="Search and Select a Video" onClose={sdk.close} />
+      <div className="p-10">
+        <div className="flex items-center justify-center max-w-2xl mx-auto gap-x-2">
+          <TextInput ref={inputRef} className="w-10/12" placeholder="Search for a video..." />
+          <Button
+            variant="primary"
+            className="w-2/12 !font-bold"
+            onClick={onSearch}
+            isLoading={yt.isFetching}
+          >
+            {!yt.isFetching ? 'SEARCH' : ''}
+          </Button>
+        </div>
+        <br />
+        <br />
+        {yt.results.length > 0 && (
+          <ul className="mx-auto grid max-w-5xl grid-cols-2 justify-items-center gap-x-2 gap-y-10 sm:grid-cols-3 md:grid-cols-4">
+            {yt.results.map((video) => (
+              <motion.li key={video.id.videoId} initial="hidden" animate="show" variants={item}>
+                <SmallTile
+                  id={video.id.videoId}
+                  thumbnail={video.snippet.thumbnails.medium?.url}
+                  title={video.snippet.title}
+                  date={video.snippet.publishedAt}
+                  onSelect={onSelectVideo}
+                  onPreview={onModalLauncher}
+                />
+              </motion.li>
+            ))}
+          </ul>
+        )}
+        {yt.hasLoadMore && (
+          <Button
+            className="!block mx-auto my-10 !font-bold !min-w-[200px]"
+            variant="primary"
+            onClick={onLoadMore}
+            isLoading={yt.isFetching}
+          >
+            {!yt.isFetching ? 'LOAD MORE' : ''}
+          </Button>
+        )}
       </div>
-      <br />
-      <br />
-      <div className="mx-auto grid max-w-5xl grid-cols-2 justify-items-center gap-x-2 gap-y-10 sm:grid-cols-3 md:grid-cols-4">
-        {yt.results.map((video) => (
-          <SmallTile
-            id={video.id.videoId}
-            thumbnail={video.snippet.thumbnails.medium?.url}
-            title={video.snippet.title}
-            date={video.snippet.publishedAt}
-            onClick={(_id) => alert(_id)}
-          />
-        ))}
-      </div>
-      {yt.hasLoadMore && (
-        <Button
-          className="!block mx-auto my-10 !font-bold !min-w-[200px]"
-          variant="negative"
-          onClick={onLoadMore}
-          isLoading={yt.isFetching}
-        >
-          {!yt.isFetching ? 'LOAD MORE' : ''}
-        </Button>
-      )}
-    </div>
+    </>
   );
 };
 
